0014b1ed79b391660584d799837cd8b5eb53421d
Sven118 <sven118@hotmail.com>
2016-11-20 14:27:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,32 +6,30 @@
  * generally a good thing. Anything you *want* to be globally visible,
  * add it to the "exports" object.
  */
- 
-(function(exports)
-{
-	// set up the render loop
-	var sim = new altspace.utilities.Simulation();
-	sim.camera.position.set(0,0,5);
-	
-	if(altspace.inClient)
-	{
-		// convert scene to meter scale from pixel scale
-		altspace.getEnclosure().then(function(enc){
-			sim.scene.scale.setScalar(enc.pixelsPerMeter);
-		});
-	}
-	
-	// create a new purple box
-	var box = new THREE.Mesh(
-		new THREE.BoxGeometry(1,1,1),
-		new THREE.MeshBasicMaterial({color: 0x441877})
-	);
-	
-	// add the box to the scene
-	sim.scene.add(box);
-	
-	// export important objects
-	exports.sim = sim;
-	exports.box = box;
+
+(function (exports) {
+    // set up the render loop
+    var sim = new altspace.utilities.Simulation();
+    sim.camera.position.set(0, 0, 5);
+
+    if (altspace.inClient) {
+        // convert scene to meter scale from pixel scale
+        altspace.getEnclosure().then(function (enc) {
+            sim.scene.scale.setScalar(enc.pixelsPerMeter);
+        });
+    }
+
+    // create planets
+    var geometry = new THREE.SphereGeometry(250, 32, 32);
+    var material = new THREE.MeshBasicMaterial({ transparent: false, map: THREE.ImageUtils.loadTexture('https://textures.forrest.cz/textures/library/maps/Earth-hires.jpg') });
+    var sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
+
+    // add the box to the scene
+    sim.scene.add(box);
+
+    // export important objects
+    exports.sim = sim;
+    exports.box = box;
 }
 )(window.MyProject = window.MyProject || {});
